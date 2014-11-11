52480fa26723198ad5339d9ce25edf701f6a35a7
Andrew J B <ajbtee@gmail.com>
2014-11-11 14:04:26 -0500

    level gen complete

--- app/src/main/java/com/detroitlabs/trimbol/GridHandler.java
+++ app/src/main/java/com/detroitlabs/trimbol/GridHandler.java
@@ -3,9 +3,6 @@ package com.detroitlabs.trimbol;
 import java.util.ArrayList;
 import java.util.Collections;
 
-/**
- * Created by andrewjb on 11/8/14.
- */
 public class GridHandler {
 
     // Symbol constants
@@ -14,9 +11,13 @@ public class GridHandler {
     final static int PAP = 2;
     final static int SCI = 3;
 
-    //  ┌─────────────────────────────────────┐
-    //  │ Generate new puzzle                 │
-    //  └─────────────────────────────────────┘
+    private static int rocSat;
+    private static int papSat;
+    private static int sciSat;
+
+    //  ┌─────────────────────────────────────────────────────┐
+    //  │ Generate new puzzle                                 │
+    //  └─────────────────────────────────────────────────────┘
     public static void initiatePuzzle(Grid grid) {
         boolean gridFull = false;
 
@@ -36,7 +37,7 @@ public class GridHandler {
                     if (empty > 0) break;
                 }
             }
-            System.out.println("***"+empty+" x"+cursorX+" y"+cursorY);
+            System.out.println("empty="+empty+" cursorX="+cursorX+" cursorY="+cursorY);
             if (empty == 0) gridFull = true;
 
             // Find a symbol and move it
@@ -44,81 +45,136 @@ public class GridHandler {
             do {
                 cursorX = (int) Math.floor(Math.random() * grid.getGridX());
                 cursorY = (int) Math.floor(Math.random() * grid.getGridY());
+                symbol = grid.grid[cursorY][cursorX];
 
-                // Once found, check all 4 directions randomly for an empty space
+                // From cursor check all 4 directions randomly for an empty space
                 if (grid.grid[cursorY][cursorX] != NIL){
-                    foundSymbol = true;
-                    // Create an arraylist of directions and randomize it
-                    ArrayList<Integer> directions = new ArrayList<Integer>();
-                    for(int i=1;i<=4;i++) { directions.add(i); }
-                    Collections.shuffle(directions);
 
-                    // Step through that list
-                    for(int i=0;i<4;i++){
-                        // Check north
-                        if (directions.get(i) == 1){
-                            if (cursorY - 1 > -1 && grid.grid[cursorY - 1][cursorX] == NIL){
-                                grid.grid[cursorY - 1][cursorX] = symbol;
-                                break;
-                            }
-                        }
-                        // Check south
-                        if (directions.get(i) == 2){
-                            if (cursorY + 1 < grid.getGridY() && grid.grid[cursorY + 1][cursorX] == NIL){
-                                grid.grid[cursorY + 1][cursorX] = symbol;
-                                break;
-                            }
-                        }
-                        // Check west
-                        if (directions.get(i) == 3){
-                            if (cursorX - 1 > -1 && grid.grid[cursorY][cursorX - 1] == NIL){
-                                grid.grid[cursorY][cursorX - 1] = symbol;
-                                break;
-                            }
-                        }
-                        // Check east
-                        if (directions.get(i) == 4){
-                            if (cursorX + 1 < grid.getGridX() && grid.grid[cursorY][cursorX + 1] == NIL){
-                                grid.grid[cursorY][cursorX + 1] = symbol;
-                                break;
-                            }
-                        }
-                    }
+//                    boolean placedSymbol = false;
+//                    if (symbol == ROC && rocSat <= (papSat+sciSat+1)/2 && !placedSymbol) {
+//                        foundSymbol = true;
+//                        pickDirection(grid, cursorX, cursorY, symbol);
+//                        placedSymbol = true;
+//                    }
+//                    if (symbol == PAP && papSat <= (rocSat+sciSat+1)/2 && !placedSymbol) {
+//                        foundSymbol = true;
+//                        pickDirection(grid, cursorX, cursorY, symbol);
+//                        placedSymbol = true;
+//                    }
+//                    if (symbol == SCI && sciSat <= (rocSat+papSat+1)/2 && !placedSymbol) {
+//                        foundSymbol = true;
+//                        pickDirection(grid, cursorX, cursorY, symbol);
+//                        placedSymbol = true;
+//                    }
+
+                    foundSymbol = true;
+                    pickDirection(grid, cursorX, cursorY, symbol);
+
                 }
             } while (!foundSymbol);
         }
     }
 
-    //  ┌─────────────────────────────────────┐
-    //  │ Print current grid to console       │
-    //  └─────────────────────────────────────┘
+    //  ┌─────────────────────────────────────────────────────┐
+    //  │ Pick random direction for new symbol                │
+    //  └─────────────────────────────────────────────────────┘
+    private static void pickDirection(Grid grid, int cursorX, int cursorY, int symbol) {
+
+        // Create an arraylist of directions and randomize it
+        ArrayList<Integer> directions = new ArrayList<Integer>();
+        for(int i=1;i<=4;i++) { directions.add(i); }
+        Collections.shuffle(directions);
+
+        // Step through that list
+        for(int i=0;i<4;i++){
+            // Check north
+            if (directions.get(i) == 1){
+                if (cursorY - 1 > -1 && grid.grid[cursorY - 1][cursorX] == NIL){
+                    grid.grid[cursorY - 1][cursorX] = symbol;
+                    pickCounter(grid, cursorX, cursorY, symbol);
+                    break;
+                }
+            }
+            // Check south
+            if (directions.get(i) == 2){
+                if (cursorY + 1 < grid.getGridY() && grid.grid[cursorY + 1][cursorX] == NIL){
+                    grid.grid[cursorY + 1][cursorX] = symbol;
+                    pickCounter(grid, cursorX, cursorY, symbol);
+                    break;
+                }
+            }
+            // Check west
+            if (directions.get(i) == 3){
+                if (cursorX - 1 > -1 && grid.grid[cursorY][cursorX - 1] == NIL){
+                    grid.grid[cursorY][cursorX - 1] = symbol;
+                    pickCounter(grid, cursorX, cursorY, symbol);
+                    break;
+                }
+            }
+            // Check east
+            if (directions.get(i) == 4){
+                if (cursorX + 1 < grid.getGridX() && grid.grid[cursorY][cursorX + 1] == NIL){
+                    grid.grid[cursorY][cursorX + 1] = symbol;
+                    pickCounter(grid, cursorX, cursorY, symbol);
+                    break;
+                }
+            }
+        }
+    }
+
+    //  ┌─────────────────────────────────────────────────────┐
+    //  │ Place counter symbol                                │
+    //  └─────────────────────────────────────────────────────┘
+    private static void pickCounter(Grid grid, int cursorX, int cursorY, int symbol) {
+        if (symbol == ROC) {
+            grid.grid[cursorY][cursorX] = SCI;
+            sciSat++;
+        }
+        if (symbol == SCI) {
+            grid.grid[cursorY][cursorX] = PAP;
+            papSat++;
+        }
+        if (symbol == PAP) {
+            grid.grid[cursorY][cursorX] = ROC;
+            rocSat++;
+        }
+    }
+
+    //  ┌─────────────────────────────────────────────────────┐
+    //  │ Print current grid to console                       │
+    //  └─────────────────────────────────────────────────────┘
     public static void printGrid(Grid grid) {
 
+        // Symbol saturation
+        System.out.println("roc: " + rocSat + " pap: " + papSat + " sci: " + sciSat);
+
         // X coordinates
-        System.out.print("\n\n" + grid.getGridX() + grid.getGridY() + "|");
+        System.out.print("\n\n" + grid.getGridX() + grid.getGridY() + "╷");
         for (int x = 0; x < grid.getGridX(); x++)
             System.out.print(" " + x);
-        System.out.print("\n--+");
+        System.out.print("\n╶─┼");
         for (int x = 0; x < grid.getGridX(); x++)
-            System.out.print("--");
+            System.out.print("──");
+        System.out.print("─┐");
 
         // Y coordinates
         for (int y = 0; y < grid.getGridY(); y++){
-            System.out.print("\n" + y + " | ");
-            // print a row
+            System.out.print("\n" + y + " │ ");
+            // Print a row
             for (int x = 0; x < grid.getGridX(); x++){
                 if (grid.grid[y][x] == 0) System.out.print(". ");
                 if (grid.grid[y][x] == 1) System.out.print("R ");
                 if (grid.grid[y][x] == 2) System.out.print("P ");
                 if (grid.grid[y][x] == 3) System.out.print("S ");
             }
+            System.out.print("│");
         }
 
         // End print
-        System.out.print("\n--+");
+        System.out.print("\n╶─┴");
         for (int x = 0; x < grid.getGridX(); x++)
-            System.out.print("--");
-        System.out.println("\nEND PRINT");
+            System.out.print("──");
+        System.out.print("─┘");
+        System.out.println("\n════════════════════════");
     }
-
 }
