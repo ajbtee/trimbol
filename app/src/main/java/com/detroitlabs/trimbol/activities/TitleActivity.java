7c9daa01dfccdcc7addb52a210a6c1778e398b42
Andrew J B <ajbtee@gmail.com>
2014-12-09 16:59:34 -0500

    title animations

--- app/src/main/java/com/detroitlabs/trimbol/activities/TitleActivity.java
+++ app/src/main/java/com/detroitlabs/trimbol/activities/TitleActivity.java
@@ -1,5 +1,6 @@
 package com.detroitlabs.trimbol.activities;
 
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
@@ -7,7 +8,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.detroitlabs.trimbol.R;
 import com.detroitlabs.trimbol.utils.ThemeGen;
@@ -15,18 +18,33 @@ import com.detroitlabs.trimbol.utils.ThemeGen;
 
 public class TitleActivity extends Activity {
 
+    View playButton;
+    View tutorialButton;
+    View optionsButton;
+
+    LinearLayout.LayoutParams playParams;
+    LinearLayout.LayoutParams tutorialParams;
+    LinearLayout.LayoutParams optionsParams;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition(0, 0);
         setContentView(R.layout.activity_title);
 
+        playButton = findViewById(R.id.play);
+        tutorialButton = findViewById(R.id.tutorial);
+        optionsButton = findViewById(R.id.options);
+
+        playParams = (LinearLayout.LayoutParams) playButton.getLayoutParams();
+        tutorialParams = (LinearLayout.LayoutParams) tutorialButton.getLayoutParams();
+        optionsParams = (LinearLayout.LayoutParams) optionsButton.getLayoutParams();
+
         ThemeGen.makePaints(getBaseContext());
         getWindow().getDecorView().setBackgroundColor(Color.parseColor(ThemeGen.background));
         ImageView title = (ImageView) findViewById(R.id.trimbol_title);
         ThemeGen.setTitle(title);
 
-        View playButton = findViewById(R.id.play);
         playButton.setClickable(true);
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +53,16 @@ public class TitleActivity extends Activity {
                 startActivity(intent);
             }
         });
-        View optionsButton = findViewById(R.id.options);
+
+        tutorialButton.setClickable(true);
+        tutorialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PuzzleActivity.class);
+                startActivity(intent);
+            }
+        });
+
         optionsButton.setClickable(true);
         optionsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +71,52 @@ public class TitleActivity extends Activity {
                 startActivity(intent);
             }
         });
+        animateButtons();
+    }
+
+    private void animateButtons() {
+        final int playMargin = playParams.leftMargin;
+        final int tutorialMargin = tutorialParams.leftMargin;
+        final int optionsMargin = optionsParams.leftMargin;
+
+        ValueAnimator animatorPlay = ValueAnimator.ofFloat(1,0f);
+        animatorPlay.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float val = (Float) valueAnimator.getAnimatedValue();
+                playParams.leftMargin = (int) (val*300)+playMargin;
+                playButton.setLayoutParams(playParams);
+            }
+        });
+        animatorPlay.setDuration(200);
+        animatorPlay.setInterpolator(new DecelerateInterpolator());
+        animatorPlay.start();
+
+        ValueAnimator animatorTutorial = ValueAnimator.ofFloat(1,0f);
+        animatorTutorial.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float val = (Float) valueAnimator.getAnimatedValue();
+                tutorialParams.leftMargin = (int) (val*300)+tutorialMargin;
+                tutorialButton.setLayoutParams(tutorialParams);
+            }
+        });
+        animatorTutorial.setDuration(300);
+        animatorTutorial.setInterpolator(new DecelerateInterpolator());
+        animatorTutorial.start();
+
+        ValueAnimator animatorOptions = ValueAnimator.ofFloat(1,0f);
+        animatorOptions.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float val = (Float) valueAnimator.getAnimatedValue();
+                optionsParams.leftMargin = (int) (val*300)+optionsMargin;
+                optionsButton.setLayoutParams(optionsParams);
+            }
+        });
+        animatorOptions.setDuration(400);
+        animatorOptions.setInterpolator(new DecelerateInterpolator());
+        animatorOptions.start();
     }
 
     @Override
