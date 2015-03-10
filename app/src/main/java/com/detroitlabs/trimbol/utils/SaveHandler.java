95db46186a31105348514e4cdee40db4fe74014f
nathan dotz <nathan.dotz@gmail.com>
2015-03-10 09:09:58 -0400

    Log instead of print
    Any time a class is already coupled to Android framework

---
+++ app/src/main/java/com/detroitlabs/trimbol/utils/SaveHandler.java
@@ -0,0 +1,27 @@
+package com.detroitlabs.trimbol.utils;
+
+import android.app.Activity;
+import android.content.Context;
+
+import android.util.Log;
+import com.detroitlabs.trimbol.objects.GameBoard;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class SaveHandler extends Activity {
+
+    public void saveGame(GameBoard gameBoard) {
+        String SAVEFILE = "trimbol_save";
+        String data = gameBoard.getGrid().toString();
+
+        try {
+            FileOutputStream fos = openFileOutput(SAVEFILE, Context.MODE_PRIVATE);
+            fos.write(data.getBytes());
+            fos.close();
+        } catch (IOException e) {
+            Log.e(e.getMessage();
+        }
+    }
+
+}
