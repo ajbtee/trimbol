d144d2d65ee95975dd6df380ab745f8f256d38b9
Andrew Beattie <ajbtee@gmail.com>
2017-04-02 18:22:24 -0400

    AB : adding rotate canvas rendered Rounded Hex

--- app/src/main/java/com/detroitlabs/trimbol/prototype/HexView.java
+++ app/src/main/java/com/detroitlabs/trimbol/prototype/HexView.java
@@ -7,15 +7,16 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
 import android.graphics.RectF;
+import android.support.annotation.FloatRange;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class HexView extends View {
 
-    private static final float CORNER_RADIUS = 0.36f;
+    private static final float ROTATOR = 0f;
+    private static final float CORNER_RADIUS = 0.1f;
     private static final float SYMBOL_SIZE = 0.9f;
 
-    private int colorFace;
     private Paint paintFace = new Paint();
     private Paint paintDepth = new Paint();
 
@@ -40,73 +41,75 @@ public class HexView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        float halfWidth = getWidth()/2;
-        float halfHeight = getHeight()/2;
-        float radius = (halfWidth <= halfHeight ? halfWidth : halfHeight) * SYMBOL_SIZE;
-        Point center = new Point(Math.round(halfWidth), Math.round(halfHeight));
+        float radius = (getWidth()/2 <= getHeight()/2 ? getWidth()/2 : getHeight()/2) * SYMBOL_SIZE;
 
-        drawRoundHex(center, radius, canvas);
-        invalidate();
+        for (int i = 0; i < 6; i++) {
+            canvas.save();
+            canvas.rotate((i * 60) + ROTATOR, getWidth()/2, getHeight()/2);
+            drawRoundHex(radius, canvas);
+            canvas.restore();
+        }
     }
 
-    private void drawRoundHex(Point center, float radius, Canvas canvas) {
+    private void drawRoundHex(float radius, Canvas canvas) {
         Point target = new Point(
-                (int) (center.x + (-radius * (1 - 2 * CORNER_RADIUS) / 2)),
-                (int) (center.y + Math.sqrt(3 * radius) * (1 - 2 * CORNER_RADIUS)));
+                (int) (getWidth()/2 + (-radius * (1 - 2 * CORNER_RADIUS) / 2)),
+                (int) (getHeight()/2 - Math.sqrt(3) * radius * (1 - 2 * CORNER_RADIUS) / 2));
 
         RectF arcRect = new RectF(
-                (int) (target.x - Math.sqrt(3 * (CORNER_RADIUS * radius))),
-                (int) (target.y - Math.sqrt(3 * (CORNER_RADIUS * radius))),
-                (int) (target.x + Math.sqrt(3 * (CORNER_RADIUS * radius))),
-                (int) (target.y + Math.sqrt(3 * (CORNER_RADIUS * radius))));
+                (int) (target.x - Math.sqrt(3) * (CORNER_RADIUS * radius)),
+                (int) (target.y - Math.sqrt(3) * (CORNER_RADIUS * radius)),
+                (int) (target.x + Math.sqrt(3) * (CORNER_RADIUS * radius)),
+                (int) (target.y + Math.sqrt(3) * (CORNER_RADIUS * radius)));
 
-        float baseAngle = getAngle(center, target);
+        float baseAngle = getAngle(target);
 
         Path arc = new Path();
-        arc.arcTo(arcRect, baseAngle - 30, 60);
+        arc.arcTo(arcRect, (360 - baseAngle) - 30, 60);
+        arc.rLineTo((1 - 2 * CORNER_RADIUS) * radius, 0);
 
-        drawDebug(radius, center, target, arcRect, canvas);
         canvas.drawPath(arc, paintFace);
     }
 
-    private float getAngle(Point center, Point target) {
-        float deltaX = target.x - center.x;
-        float deltaY = target.y - center.y;
-        float angle = (float) Math.atan2(deltaX, deltaY);
+    @FloatRange(from = 0, to = 360)
+    private float getAngle(Point target) {
+        float deltaX = target.x - getWidth()/2;
+        float deltaY = -(target.y - getHeight()/2);
+        float angle = (float) Math.atan2(deltaY, deltaX);
 
-        if (angle < 0) { angle += 360; }
+        if (angle < 0) { angle += Math.PI / 2; }
 
-        return angle;
+        return (float) (angle * 360 / (2 * Math.PI));
     }
 
 
-    private void drawDebug(float radius, Point center, Point target, RectF arcRect, Canvas canvas) {
-        Paint paintDebug = new Paint();
-        paintDebug.setColor(Color.parseColor("#d2d2d2"));
-        paintDebug.setStrokeWidth(2);
-        paintDebug.setAntiAlias(true);
-        paintDebug.setStyle(Paint.Style.STROKE);
-
-        // base angle radius
-        for (int i=0; i<6; i++) {
-            float angle_deg = 60 * i;
-            float angle_rad = (float) Math.PI / 180 * angle_deg;
-            Point angledRadius = new Point(
-                    (int) (center.x + radius * (float) Math.cos(angle_rad)),
-                    (int) (center.y + radius * (float) Math.sin(angle_rad)));
-            canvas.drawLine(center.x, center.y, angledRadius.x, angledRadius.y, paintDebug);
-        }
-
-        // arc rectangle
-        canvas.drawRect(arcRect, paintDebug);
-
-        // base angle target
-        canvas.drawLine(center.x, center.y, target.x, target.y, paintDepth);
-
-    }
+//    private void drawDebug(float radius, Point center, Point target, RectF arcRect, Canvas canvas) {
+//        Paint paintDebug = new Paint();
+//        paintDebug.setColor(Color.parseColor("#d2d2d2"));
+//        paintDebug.setStrokeWidth(2);
+//        paintDebug.setAntiAlias(true);
+//        paintDebug.setStyle(Paint.Style.STROKE);
+//
+//        // base angle radius
+//        for (int i=0; i<6; i++) {
+//            float angle_deg = 60 * i;
+//            float angle_rad = (float) Math.PI / 180 * angle_deg;
+//            Point angledRadius = new Point(
+//                    (int) (center.x + radius * (float) Math.cos(angle_rad)),
+//                    (int) (center.y + radius * (float) Math.sin(angle_rad)));
+//            canvas.drawLine(center.x, center.y, angledRadius.x, angledRadius.y, paintDebug);
+//        }
+//
+//        // arc rectangle
+//        canvas.drawRect(arcRect, paintDebug);
+//
+//        // base angle target
+//        canvas.drawLine(center.x, center.y, target.x, target.y, paintDepth);
+//
+//    }
 
     private void init() {
-        colorFace = Color.parseColor("#6c9b45");
+        int colorFace = Color.parseColor("#6c9b45");
         int depthColorOffset = 60;
 
         paintDepth.setARGB(255, Math.abs(Color.red(colorFace) + depthColorOffset), Math.abs(Color.green(colorFace) + depthColorOffset), Math.abs(Color.blue(colorFace) + depthColorOffset));
